21b2422c11e09ad45b1c29676cef99e4982276a0
Marcus Rådell <marcus.radell@sveasolar.com>
2020-12-10 23:30:09 +0100

    add post endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, post, App, HttpResponse, HttpServer, Responder};
 
 #[get("/ready")]
 async fn ready() -> impl Responder {
@@ -17,11 +17,24 @@ async fn hello() -> impl Responder {
         .body("<H1>Hello Monadium!</H2>")
 }
 
+#[post("/api/identity")]
+async fn identity_controller() -> impl Responder {
+    HttpResponse::Ok()
+        .content_type("application/json")
+        .body("{\"ok\": false, \"error\": {\"code\": \"failed\"}}")
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     println!("Starting server...");
-    HttpServer::new(|| App::new().service(hello).service(live).service(ready))
-        .bind("0.0.0.0:8080")?
-        .run()
-        .await
+    HttpServer::new(|| {
+        App::new()
+            .service(identity_controller)
+            .service(hello)
+            .service(live)
+            .service(ready)
+    })
+    .bind("0.0.0.0:8080")?
+    .run()
+    .await
 }
