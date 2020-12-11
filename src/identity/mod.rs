8e2f32d1750e516ac9d9419628e2842077bd611a
Marcus Rådell <marcus.radell@sveasolar.com>
2020-12-12 00:02:02 +0100

    impl serialize

--- src/identity/mod.rs
+++ src/identity/mod.rs
@@ -1,24 +1,15 @@
 use actix_web::{web, HttpResponse, Responder};
-use std::fmt;
+use serde;
 
+#[derive(serde::Serialize)]
 pub struct Err {
     code: &'static str,
 }
 
-impl fmt::Display for Err {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "\"code\": {}", self.code)
-    }
-}
+#[derive(serde::Serialize)]
 pub struct SignUpWithEmailResult {
     ok: bool,
     error: Err,
-}
-
-impl fmt::Display for SignUpWithEmailResult {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{{\"ok\": {}, \"error\": {}}}", self.ok, self.error)
-    }
 }
 
 pub async fn sign_up() -> impl Responder {
@@ -29,7 +20,7 @@ pub async fn sign_up() -> impl Responder {
 
     HttpResponse::Ok()
         .content_type("application/json")
-        .body(result.to_string())
+        .body(serde_json::to_string(&result).unwrap())
 }
 
 pub fn schema(cfg: &mut web::ServiceConfig) {
