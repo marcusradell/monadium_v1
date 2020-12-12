a7e8e69ed9ac9ee16e9095e4e9b173e8a86dcabc
Marcus Rådell <marcus.radell@sveasolar.com>
2020-12-12 23:12:16 +0100

    add /status and serialize

--- src/health/mod.rs
+++ src/health/mod.rs
@@ -1,24 +1,32 @@
 use actix_web::{web, HttpResponse, Responder};
+use serde;
 
-async fn ready() -> impl Responder {
-    HttpResponse::Ok()
+#[derive(serde::Serialize)]
+enum Status {
+    Live,
+    Ready,
 }
 
 async fn live() -> impl Responder {
     HttpResponse::Ok()
 }
 
-async fn status() -> impl Responder {
+async fn ready() -> impl Responder {
     HttpResponse::Ok()
-        .content_type("text/html")
-        .body("<H1>Hello Monadium!</H2>")
+}
+
+async fn status() -> impl Responder {
+    let status = Status::Ready;
+    HttpResponse::Ok()
+        .content_type("application/json")
+        .body(serde_json::to_string(&status).unwrap())
 }
 
 pub fn schema(cfg: &mut web::ServiceConfig) {
     cfg.service(
         web::scope("/health")
-            .service(web::resource("/status").route(web::get().to(status)))
             .service(web::resource("/live").route(web::get().to(live)))
-            .service(web::resource("/ready").route(web::get().to(ready))),
+            .service(web::resource("/ready").route(web::get().to(ready)))
+            .service(web::resource("/status").route(web::get().to(status))),
     );
 }
