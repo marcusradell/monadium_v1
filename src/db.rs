92dcb6fb9d4fc290fcd645c50b18ae159cfd2daa
Marcus Rådell <marcus.radell@sveasolar.com>
2021-02-06 21:45:18 +0100

    refactor module namespaces

--- src/db.rs
+++ src/db.rs
@@ -1,18 +1,19 @@
 use crate::models;
 use crate::schema::identity::dsl;
-use diesel::pg::PgConnection;
+use diesel::pg;
 use diesel::prelude::*;
 use dotenv::dotenv;
 use std::env;
 
-pub fn establish_connection() -> PgConnection {
+pub fn establish_connection() -> pg::PgConnection {
     dotenv().ok();
 
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-    PgConnection::establish(&database_url).expect(&format!("Error connecting to {}", database_url))
+    pg::PgConnection::establish(&database_url)
+        .expect(&format!("Error connecting to {}", database_url))
 }
 
-pub fn seed(connection: &PgConnection) {
+pub fn seed(connection: &pg::PgConnection) {
     // identity::create_post(&connection, "me@example.com", "##password_hash##");
 
     let result = dsl::identity
