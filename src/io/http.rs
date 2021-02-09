dd30c823829297bfd3fc3fee4cc51b6e45894413
Marcus Rådell <marcus.radell@sveasolar.com>
2021-02-09 08:48:16 +0100

    fix dangling await

--- src/io/http.rs
+++ src/io/http.rs
@@ -4,7 +4,7 @@ use crate::io::db;
 use actix_web::{middleware, web, App, HttpServer};
 
 pub async fn init(db_pool: db::Pool) -> std::io::Result<()> {
-    HttpServer::new(move || {
+    let server = HttpServer::new(move || {
         App::new()
             .data(db_pool.clone())
             .wrap(middleware::Logger::default())
@@ -19,5 +19,5 @@ pub async fn init(db_pool: db::Pool) -> std::io::Result<()> {
 
     println!("Server started.");
 
-    Ok(())
+    server.await
 }
