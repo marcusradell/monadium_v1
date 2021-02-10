5f2aee5ccb21a1281e5668047129e216f6211a6a
Marcus Rådell <marcus.radell@sveasolar.com>
2021-02-10 22:21:44 +0100

    remove db max size as the cloud run limit should be 100 per instance

--- src/io/db.rs
+++ src/io/db.rs
@@ -11,7 +11,6 @@ pub fn init() -> r2d2::Pool<ConnectionManager<PgConnection>> {
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
     let manager = ConnectionManager::<PgConnection>::new(database_url);
     r2d2::Pool::builder()
-        .max_size(2)
         .build(manager)
         .expect("Failed to create DB pool.")
 }
