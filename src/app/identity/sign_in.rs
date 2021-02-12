adff545194899aedc1638b3bd6538afc9e70782f
Marcus Rådell <marcus@radell.net>
2021-02-12 22:09:06 +0100

    implement sign in

--- src/app/identity/sign_in.rs
+++ src/app/identity/sign_in.rs
@@ -1,8 +1,11 @@
+use super::models;
+use super::schema::identity::dsl;
 use crate::io::db;
 use crate::io::jwt;
 use actix_web::{web, Error, HttpResponse};
+use diesel::prelude::*;
 
-#[derive(serde::Deserialize, Debug)]
+#[derive(serde::Deserialize, Debug, Clone)]
 pub struct SignInArgs {
     email: String,
     password: String,
@@ -18,26 +21,36 @@ pub async fn sign_in(
     jwt: web::Data<jwt::Jwt>,
     args: web::Json<SignInArgs>,
 ) -> Result<HttpResponse, Error> {
-    // TODO: use the database to lookup the email and then verify the password properly.
-    let _conn = pool.get().expect("Couldn't get DB connection from pool.");
+    use super::schema::identity::dsl::*;
+    let conn = pool.get().expect("Couldn't get DB connection from pool.");
 
-    let verify_result = argon2::verify_encoded("$argon2i$v=19$m=4096,t=3,p=1$5eOOzBOVnIIN42VyvQC0p1nWvYESj14vHF3npjm4O8s$Fx0Rtyr3J2En8aECSV9laaD0Zsw/loAARHQ19zIpU1M", args.password.as_bytes()).map_err(|e| {
+    let cloned_args = args.clone();
+    let identity_by_email = web::block(move || {
+        dsl::identity
+            .filter(email.eq(cloned_args.email))
+            .first::<models::Identity>(&conn)
+    })
+    .await
+    .map_err(|e| {
         eprintln!("{}", e);
         HttpResponse::InternalServerError().finish()
     })?;
 
+    let verify_result =
+        argon2::verify_encoded(&identity_by_email.password_hash, args.password.as_bytes())
+            .map_err(|e| {
+                eprintln!("{}", e);
+                HttpResponse::InternalServerError().finish()
+            })?;
+
     match verify_result {
         true => {
-            println!("You sent the 'test1' password.");
             let encoded_jwt = jwt.into_inner().encode(args.email.clone()).map_err(|e| {
                 eprintln!("{}", e);
-                return HttpResponse::InternalServerError().finish();
+                HttpResponse::InternalServerError().finish()
             })?;
             Ok(HttpResponse::Ok().json(SignInResponse { jwt: encoded_jwt }))
         }
-        false => {
-            println!("You didn't send the 'test1' password.");
-            Ok(HttpResponse::InternalServerError().finish())
-        }
+        false => Ok(HttpResponse::InternalServerError().finish()),
     }
 }
