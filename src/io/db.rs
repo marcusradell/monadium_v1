229c2f958cff3384907fc9b85d280ef87cbea29f
Marcus Rådell <marcus.radell@sveasolar.com>
2021-02-14 09:36:30 +0100

    move dotenv to main

--- src/io/db.rs
+++ src/io/db.rs
@@ -1,5 +1,4 @@
 use diesel::prelude::*;
-use dotenv::dotenv;
 use std::env;
 
 use diesel::r2d2::{self, ConnectionManager};
@@ -7,7 +6,6 @@ use diesel::r2d2::{self, ConnectionManager};
 pub type Pool = r2d2::Pool<ConnectionManager<PgConnection>>;
 
 pub fn init() -> r2d2::Pool<ConnectionManager<PgConnection>> {
-    dotenv().ok();
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
     let manager = ConnectionManager::<PgConnection>::new(database_url);
     r2d2::Pool::builder()
--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,14 @@
 extern crate diesel;
 extern crate argon2;
 extern crate dotenv;
+use dotenv::dotenv;
 mod app;
 mod io;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    dotenv().ok();
+
     let db_pool = io::db::init();
     let jwt = io::jwt::Jwt::new(String::from("todo_set_as_env_var"));
 
