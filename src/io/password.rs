a085511fd319b8b80048665149cb86489dffe121
Marcus Rådell <marcus.radell@sveasolar.com>
2021-02-21 18:29:35 +0100

    feat: implement password::verify

--- src/io/password.rs
+++ src/io/password.rs
@@ -6,8 +6,11 @@ pub fn hash(password: &String) -> Result<String, Error> {
     let salt: [u8; 32] = rand::thread_rng().gen();
     let config = Config::default();
 
-    argon2::hash_encoded(password.as_bytes(), &salt, &config).map_err(|e| {
-        eprintln!("{}", e);
-        Error::InternalServerError
-    })
+    argon2::hash_encoded(password.as_bytes(), &salt, &config)
+        .map_err(|_e| Error::InternalServerError)
+}
+
+pub fn verify(hash: &String, attempted_password: &String) -> Result<bool, Error> {
+    argon2::verify_encoded(hash, attempted_password.as_bytes())
+        .map_err(|_e| Error::InternalServerError)
 }
