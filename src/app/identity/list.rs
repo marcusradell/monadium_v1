51c8b2ace39f5485233fe0cd5ff975df548712e5
Marcus Rådell <marcus.radell@sveasolar.com>
2021-02-21 18:18:59 +0100

    fix: simplify error handling in identity::list

--- src/app/identity/list.rs
+++ src/app/identity/list.rs
@@ -1,18 +1,12 @@
 use super::model;
 use crate::io::db;
+use crate::io::error::Error;
 use crate::schema::identity::dsl;
-use actix_web::{web, HttpResponse, Result};
+use actix_web::{web, HttpResponse};
 use diesel::prelude::*;
 
-pub async fn list(pool: web::Data<db::Pool>) -> Result<HttpResponse> {
-    let conn = pool.get().expect("Couldn't get DB connection from pool.");
-
-    let result = web::block(move || dsl::identity.load::<model::Identity>(&conn))
-        .await
-        .map_err(|e| {
-            eprintln!("{}", e);
-            HttpResponse::InternalServerError().finish()
-        })?;
-
+pub async fn list(pool: web::Data<db::Pool>) -> Result<HttpResponse, Error> {
+    let conn = pool.get()?;
+    let result = web::block(move || dsl::identity.load::<model::Identity>(&conn)).await?;
     Ok(HttpResponse::Ok().json(result))
 }
