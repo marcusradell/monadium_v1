b05e88ecbaebb63326481cfbc84f2926713fb09a
Marcus Rådell <marcus.radell@sveasolar.com>
2021-03-09 22:00:06 +0100

    Comment out broken code.

--- src/main.rs
+++ src/main.rs
@@ -20,28 +20,28 @@ async fn main() -> std::io::Result<()> {
 
     let jwt = io::jwt::Jwt::new();
 
-    let admin_email = env::var("ADMIN_EMAIL").expect("Missing env ADMIN_EMAIL.");
+    // let admin_email = env::var("ADMIN_EMAIL").expect("Missing env ADMIN_EMAIL.");
 
-    let admin_identity = app::identity::show::show(
-        web::Data::new(db_pool.clone()),
-        web::Query::from_query(&format!("email={}", admin_email)).unwrap(),
-    )
-    .await;
+    // let admin_identity = app::identity::show::show(
+    //     web::Data::new(db_pool.clone()),
+    //     web::Query::from_query(&format!("email={}", admin_email)).unwrap(),
+    // )
+    // .await;
 
-    match admin_identity {
-        Err(_) => {
-            app::identity::new::new_admin(
-                db_pool.clone(),
-                app::identity::new::SignUpArgs {
-                    email: admin_email,
-                    password: env::var("ADMIN_PASSWORD").expect("Missing env ADMIN_PASSWORD."),
-                },
-            )
-            .await
-            .unwrap();
-        }
-        _ => {}
-    };
+    // match admin_identity {
+    //     Err(_) => {
+    //         app::identity::new::new_admin(
+    //             db_pool.clone(),
+    //             app::identity::new::SignUpArgs {
+    //                 email: admin_email,
+    //                 password: env::var("ADMIN_PASSWORD").expect("Missing env ADMIN_PASSWORD."),
+    //             },
+    //         )
+    //         .await
+    //         .unwrap();
+    //     }
+    //     _ => {}
+    // };
 
     io::http::init(
         db_pool,
