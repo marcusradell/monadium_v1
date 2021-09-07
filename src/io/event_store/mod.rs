e731e95cb682303aa746e8487a3e755895943a7d
Marcus Rådell <marcus@radell.net>
2021-09-07 13:31:15 +0200

    feat: implement EventStoreMock.add

---
+++ src/io/event_store/mod.rs
@@ -0,0 +1,31 @@
+use super::result::Result;
+use chrono::{DateTime, Utc};
+use serde::{Deserialize, Serialize};
+use sqlx::types::Json;
+use uuid::Uuid;
+
+pub mod mock;
+
+#[derive(sqlx::FromRow, Deserialize, Serialize, Debug, Clone, PartialEq, Eq)]
+pub struct Event<T: Clone> {
+    sequence_num: i64,
+    stream_id: uuid::Uuid,
+    version: i32,
+    #[serde(rename = "type")]
+    #[sqlx(rename = "type")]
+    event_type: String,
+    data: Json<T>,
+    meta: Json<EventMeta>,
+    inserted_at: DateTime<Utc>,
+}
+
+#[derive(Serialize, Deserialize, sqlx::FromRow, Debug, Clone, PartialEq, Eq)]
+pub struct EventMeta {
+    pub cid: Uuid,
+}
+
+pub trait EventStorer<T: Clone> {
+    fn add(&mut self, event: Event<T>) -> Result<()>;
+
+    fn list(&self) -> Result<Vec<Event<T>>>;
+}
