1cdc9c887ad17d01380191e3eddaec3a39d21e38
Marcus Rådell <marcus@radell.net>
2021-10-01 22:32:44 +0200

    tests: identities/create create_owner

--- src/domain/identities/create.rs
+++ src/domain/identities/create.rs
@@ -174,4 +174,37 @@ mod test {
             })
         )
     }
+
+    #[actix_rt::test]
+    async fn create_owner() {
+        let jwt = Jwt::from_secret("my_kids_will_never_get_to_play_hockey");
+        let mut repo = RepoMock::new();
+        let now = Utc::now().timestamp();
+        let id = Uuid::from_u128(100);
+
+        let result = handler(
+            Args {
+                email: "created_owner@example.com".into(),
+                password: "000".into(),
+            },
+            "created_owner@example.com",
+            "000",
+            jwt.clone(),
+            &mut repo,
+            now,
+            id,
+        )
+        .await
+        .and_then(|response| jwt.decode(response.jwt));
+
+        assert_eq!(
+            result,
+            Ok(Claims {
+                email: "created_owner@example.com".into(),
+                id,
+                exp: now + 15 * 60,
+                role: "OWNER".into()
+            })
+        )
+    }
 }
