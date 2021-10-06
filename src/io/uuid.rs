acc6b3ce3eba48aef32a591eb9adfda00775db2d
Marcus Rådell <marcus@radell.net>
2021-10-07 00:54:28 +0200

    feat: (wip) uuid uses enum error codes

--- src/io/uuid.rs
+++ src/io/uuid.rs
@@ -2,7 +2,7 @@ use super::result::{ClientError, Error};
 
 impl From<uuid::Error> for Error {
     fn from(_: uuid::Error) -> Error {
-        // TODO: Don't assume too much. Find out what can actually go wrong.
-        Error::BadRequest(ClientError::new("INVALID_UUID", "Something went wrong while handling a UUID. That's all we got for you at this point. Move along."))
+        // TODO: Find out what can actually go wrong.
+        Error::BadRequest(ClientError::internal_error())
     }
 }
